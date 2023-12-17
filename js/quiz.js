c28adc14b872cca8bb61361c5e57d995def85eb3
MarLoertscher <marcel.loertscher@students.bfh.ch>
2023-12-17 16:59:33 +0100

    Setup Quizlogic

--- js/quiz.js
+++ js/quiz.js
@@ -58,24 +58,50 @@ async function fetchData(apiURL) {
 let count = 0
 var resultData = ""
 let questionCount = 0
+let points = 0
 async function play() {
     try {
         resultData = await fetchData(url);
         console.log(resultData['results']); // Use the resultData variable for further processing
         questionCount = resultData['results']['length']
-        document.getElementById("questionbox").innerHTML = resultData['results'][count]['question'] + '\n' +'True of False?' ;
-        document.getElementById("counterbox").innerHTML = 'Question ' + count + ' of ' + questionCount;
+        startQuiz()
+        setupScreen()
+        toggleTrueFalse()
     } catch (error) {
         // Handle errors if necessary
         console.error('Error:', error);
     }    
 }
 
-
-function checkAnswer(answer) {
-    
+function setupScreen(){
+    document.getElementById("questionbox").innerHTML = resultData['results'][count]['question'] + '\n' +'True of False?' ;
+    document.getElementById("counterbox").innerHTML = 'Question ' + (count+1) + ' of ' + questionCount;
 }
 
+function checkAnswer(answer) {
+    if (answer == resultData['results'][count]['correct_answer']) {
+        // alert('Correct!')
+        points += 1
+    } else {
+        // alert('Wrong!')
+    }
+    count += 1
+    if (count == questionCount) {
+        alert('You got ' + points + ' points!')
+        endRound()
+        return
+    }else{
+        setupScreen()
+    }
+}
+
+function endRound() {
+    count = 0
+    points = 0
+    document.getElementById("questionbox").innerHTML = "Want to play again?"
+    document.getElementById("counterbox").innerHTML = ""
+    toggleTrueFalse()
+}
 
 function pressTrue() {
     checkAnswer('True')
@@ -83,3 +109,27 @@ function pressTrue() {
 function pressFalse() {
     checkAnswer('False')
 }
+
+// //create a function that hides the start button and shows the quiz   
+// function startQuiz() {
+//     document.getElementById("playButton").style.display = "none";
+//     document.getElementById("quiz").style.display = "block";
+//     play()
+// }
+
+// function toggleTrueFalseOff() {
+//     document.getElementById("trueButton").style.display = "none";
+//     document.getElementById("falseButton").style.display = "none";
+// }
+// function toggleTrueFalseOn() {
+//     document.getElementById("trueButton").style.display = "true";
+//     document.getElementById("falseButton").style.display = "true";
+// }
+// // combine the two functions into one
+// function toggleTrueFalse() {
+//     if (document.getElementById("trueButton").style.display === "none") {
+//         toggleTrueFalseOn()
+//     } else {
+//         toggleTrueFalseOff()
+//     }
+// }
